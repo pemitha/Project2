08d109a6e7337e5f34465764315dd0a24e82dd1b
Ahuja-rahul <ahuja742@gmail.com>
2023-08-06 23:08:21 -0400

    Adding the spinner

---
+++ app/src/main/java/com/example/project_2/BottomFragment.kt
@@ -0,0 +1,114 @@
+package com.example.project_2
+
+
+import android.animation.ArgbEvaluator
+import android.animation.ObjectAnimator
+import android.os.Bundle
+import android.os.Handler
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.view.animation.Animation
+import android.view.animation.RotateAnimation
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.fragment.app.Fragment
+import java.text.SimpleDateFormat
+import java.util.*
+
+class BottomFragment : Fragment() {
+    private lateinit var seasonalImageView: ImageView
+    private lateinit var dateTimeTextView: TextView
+    private lateinit var topSpinner: ConstraintLayout
+    private lateinit var imageViewSpinner: ImageView
+    lateinit var colorAnimation:ObjectAnimator
+
+    private var isSpinning = false
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_bottom, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        dateTimeTextView = view.findViewById(R.id.dateTimeTextView)
+        seasonalImageView = view.findViewById(R.id.seasonalImageView)
+        topSpinner = view.findViewById(R.id.topSpinner)
+        imageViewSpinner = view.findViewById(R.id.wheelSpinner)
+
+        startSpinnerAnimation()
+        updateDateTime()
+    }
+
+    private fun startSpinnerAnimation() {
+        isSpinning = true
+        val rotateAnimation = RotateAnimation(
+            0f, 360f,
+            Animation.RELATIVE_TO_SELF, 0.5f,
+            Animation.RELATIVE_TO_SELF, 0.5f
+        )
+        rotateAnimation.duration = 1000
+        rotateAnimation.repeatCount = Animation.INFINITE
+        imageViewSpinner.startAnimation(rotateAnimation)
+    }
+
+    fun stopSpinnerAnimation() {
+        isSpinning = false
+        imageViewSpinner.clearAnimation()
+    }
+
+
+    fun setSeasonImage(imageResId: Int) {
+        // Fade in animation
+        val fadeInAnimation: ObjectAnimator =
+            ObjectAnimator.ofFloat<View>(seasonalImageView, View.ALPHA, 0f, 1f)
+        fadeInAnimation.duration = 1000 // in milliseconds
+
+
+        // Fade out animation
+        val fadeOutAnimation: ObjectAnimator =
+            ObjectAnimator.ofFloat<View>(seasonalImageView, View.ALPHA, 1f, 0f)
+        fadeOutAnimation.duration = 1000
+
+
+        fadeOutAnimation.start()
+        fadeInAnimation.start()
+        seasonalImageView.setImageResource(imageResId)
+
+
+    }
+
+
+    fun animateBackgroundColor(fromColor: Int, toColor: Int) {
+        colorAnimation = ObjectAnimator.ofObject(
+            topSpinner,
+            "backgroundColor",
+            ArgbEvaluator(),
+            fromColor,
+            toColor
+        )
+        colorAnimation.duration = 5000 // 5 seconds
+        colorAnimation.start()
+    }
+
+    private fun updateDateTime() {
+        val handler = Handler()
+        handler.post(object : Runnable {
+            override fun run() {
+                dateTimeTextView.text = getCurrentDateTime()
+                handler.postDelayed(this, 1000)
+            }
+        })
+    }
+
+    private fun getCurrentDateTime(): String {
+        val currentDate = Calendar.getInstance().time
+        val dateFormat = SimpleDateFormat("dd/MM/yyyy HH:mm:ss", Locale.getDefault())
+        return dateFormat.format(currentDate)
+    }
+}
---
+++ app/src/main/java/com/example/project_2/MainActivity.kt
@@ -0,0 +1,120 @@
+package com.example.project_2
+
+import android.media.MediaPlayer
+import android.os.Bundle
+import android.os.Handler
+import android.view.animation.Animation
+import android.view.animation.TranslateAnimation
+import android.widget.Button
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.fragment.app.FragmentTransaction
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var mediaPlayer: MediaPlayer
+    private var handler: Handler = Handler()
+    private var isRunning = false
+    private var currentSeason = 0
+    private lateinit var bottomFragment: BottomFragment
+    private lateinit var topSegment: ConstraintLayout
+    private lateinit var birdsAnimated: ImageView
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        bottomFragment = BottomFragment()
+        topSegment = findViewById(R.id.topSegment)
+        birdsAnimated = findViewById(R.id.birds)
+
+        val transaction: FragmentTransaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.bottomSegment, bottomFragment)
+        transaction.commit()
+
+        startAnimation()
+
+        val startButton = findViewById<Button>(R.id.startButton)
+        startButton.setOnClickListener {
+            startSeasonalAnimations()
+        }
+
+        val stopButton = findViewById<Button>(R.id.stopButton)
+        stopButton.setOnClickListener {
+            stopSeasonalAnimations()
+        }
+
+        mediaPlayer = MediaPlayer.create(this, R.raw.autumn_song)
+    }
+
+    private fun startSeasonalAnimations() {
+        isRunning = true
+        currentSeason = 0
+        updateSeason()
+    }
+
+    private fun stopSeasonalAnimations() {
+        isRunning = false
+        mediaPlayer.stop()
+        bottomFragment.stopSpinnerAnimation()
+        bottomFragment.animateBackgroundColor(resources.getColor(R.color.white), resources.getColor(R.color.white))
+        bottomFragment.setSeasonImage(R.drawable.autumn)
+    }
+
+    private fun updateSeason() {
+        when (currentSeason) {
+            0 -> {
+                bottomFragment.animateBackgroundColor(resources.getColor(R.color.OrangeRed), resources.getColor(R.color.darkseagreen))
+                bottomFragment.setSeasonImage(R.drawable.spring)
+                mediaPlayer.stop()
+                mediaPlayer = MediaPlayer.create(this, R.raw.spring_song)
+            }
+            1 -> {
+                bottomFragment.animateBackgroundColor(resources.getColor(R.color.darkseagreen), resources.getColor(R.color.yellow))
+                bottomFragment.setSeasonImage(R.drawable.summer)
+                mediaPlayer.stop()
+                mediaPlayer = MediaPlayer.create(this, R.raw.summer_song)
+            }
+            2 -> {
+                bottomFragment.animateBackgroundColor(resources.getColor(R.color.yellow), resources.getColor(R.color.white))
+                bottomFragment.setSeasonImage(R.drawable.autumn)
+                mediaPlayer.stop()
+                mediaPlayer = MediaPlayer.create(this, R.raw.autumn_song)
+            }
+            3 -> {
+                bottomFragment.animateBackgroundColor(resources.getColor(R.color.white), resources.getColor(R.color.OrangeRed))
+                bottomFragment.setSeasonImage(R.drawable.winter)
+                mediaPlayer.stop()
+                mediaPlayer = MediaPlayer.create(this, R.raw.winter_song)
+            }
+        }
+
+        mediaPlayer.start()
+        currentSeason = (currentSeason + 1) % 4
+        if (isRunning) {
+            if(currentSeason == 4){
+                currentSeason = 0
+            }
+            handler.postDelayed({ updateSeason() }, 5000)
+        }
+    }
+
+    private fun startAnimation() {
+
+        //Birds
+        val animation = TranslateAnimation(
+            Animation.RELATIVE_TO_PARENT, 0f,
+            Animation.RELATIVE_TO_PARENT, 1f,
+            Animation.RELATIVE_TO_SELF, 0f,
+            Animation.RELATIVE_TO_SELF, 0f
+        )
+
+        animation.duration = 3000 // Animation duration in milliseconds
+        animation.repeatMode = Animation.RESTART
+        animation.repeatCount = Animation.INFINITE
+
+        birdsAnimated.startAnimation(animation)
+    }
+}
